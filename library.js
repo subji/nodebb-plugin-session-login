6b930c83cfda7c420f448a3582f500df57073c45
subji <qkrwlrjq@gmail.com>
2017-04-05 11:16:54 +0900

    20170405111654

--- library.js
+++ library.js
@@ -7,7 +7,6 @@ var db = module.parent.require('./database'); // NodeBB / src / database 참고.
 var au = module.parent.require('./controllers/authentication'); // NodeBB / src / controllers 참고.
 
 var winston = require('winston');
-var jwt = require('jsonwebtoken');
 // 모듈 객체.
 var plugin = {};
 
@@ -20,6 +19,8 @@ plugin.init = function (params, callback)	{
 		console.log(arguments);
 	});
 
+	console.log('login init!!');
+
 	callback();
 }
 
@@ -30,6 +31,7 @@ plugin.verifyUser = function (token, callback)	{
 // 이 함수는 NodeBB 에서 어떤 동작 또는 페이지이동 때마다 호출되므로 계속해서 토큰을 받아오고 유저 유효성 검사를 할 것이다.
 // 그러므로 함수 첫줄에 세션 확인을 하는 구문을 만들어 같은 세션일 경우 유저 유효성 검사를 넘어가도록 한다.
 plugin.addMiddleware = function (req, res, next)	{
+	console.log('login add middle ware!!');
 	// 이미 있는 세션일 경우 요청 프로퍼티에 user 와 user 안에 uid 가 존재 한다.
 	// TODO.
 	// 로그아웃 후 창을 닫고 바이오클라우드에서 다시 커뮤니티로 접속하면 세션관련 리프레쉬가 발생한다.
